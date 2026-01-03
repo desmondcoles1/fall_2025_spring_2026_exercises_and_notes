d4ff79a5fe56f694c4ad6cc6d0e0fe8bce8fe4d9
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2026-01-03 12:25:12 -0600

    solved leetcode 2390

---
+++ rust_exercises/leetcode_2390/src/main.rs
@@ -0,0 +1,26 @@
+pub fn remove_stars_slow(s: String) -> String {
+    let mut stack = String::from("");
+    for ch in s.chars(){
+        if ch == '*' {
+            stack.pop();
+        }
+        else {
+            stack.push(ch);
+        }
+    }
+// this solution is slow because of the fact that strings are utf-8 encoded
+       
+// this solution directly works with the bytes. if the string had utf-8 chars there would be errors
+pub fn remove_stars_fast_unsafe(s: String) -> String {
+    let mut stack: Vec<u8> = Vec::with_capacity(s.len());
+    
+    for byte in s.as_bytes() {
+        if *byte == b'*' {
+            stack.pop();
+        } else {
+            stack.push(*byte);
+        }
+    }
+    
+    unsafe { String::from_utf8_unchecked(stack) }
+}
