637064bd3f25cc649efe4bf8ce6374401bb964e0
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2026-01-03 12:28:48 -0600

    added comments

--- rust_exercises/leetcode_2390/src/main.rs
+++ rust_exercises/leetcode_2390/src/main.rs
@@ -12,7 +12,7 @@ pub fn remove_stars_slow(s: String) -> String {
        
 // this solution directly works with the bytes. if the string had utf-8 chars there would be errors
 pub fn remove_stars_fast_unsafe(s: String) -> String {
-    let mut stack: Vec<u8> = Vec::with_capacity(s.len());
+    let mut stack: Vec<u8> = Vec::with_capacity(s.len()); //preallocates length to avoid resizing
     
     for byte in s.as_bytes() {
         if *byte == b'*' {
@@ -22,5 +22,20 @@ pub fn remove_stars_fast_unsafe(s: String) -> String {
         }
     }
     
-    unsafe { String::from_utf8_unchecked(stack) }
+    unsafe { String::from_utf8_unchecked(stack) } // faster because it doesn't check for utf-8 validity
+}
+
+//similar safe version
+pub fn remove_stars_fast_safe(s: String) -> String {
+    let mut stack: Vec<u8> = Vec::with_capacity(s.len()); //preallocates length to avoid resizing
+    
+    for byte in s.as_bytes() {
+        if *byte == b'*' {
+            stack.pop();
+        } else {
+            stack.push(*byte);
+        }
+    }
+    
+    String::from_utf8(stack).unwrap() // faster because it doesn't check for utf-8 validity
 }
